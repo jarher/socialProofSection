8332c0661569ce00936f92cb9425f4292cfb0930
jarher <jefferh33@gmail.com>
2024-05-27 17:04:23 -0500

    modifying fetch parameter for get data in repository

--- assets/js/getData.js
+++ assets/js/getData.js
@@ -1,5 +1,8 @@
 async function makeFetch() {
-  const response = await fetch("../../data.json");
+  // data url from github repository
+  const response = await fetch(
+    "https://raw.githubusercontent.com/jarher/socialProofSection/main/data.json"
+  );
   const data = await response.json();
   return data;
 }
